8392acbad0d95a524c3314d42b2b99a726317ff5
ReactReduxDeveloper <cool.pomolov@ya.ru>
2022-06-23 17:06:54 +0300

    Original Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Rating from "./Star";
+import Accordion from "./Accordion/Accordion";
+import {stringify} from "querystring";
+import OnOff from "./OnOff/OnOff";
+import UncontrolledAccordion from "./Accordion/UncontrolledAccordion";
+import UncontrolledRating from "./Star";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className={"App"}>
+            <OnOff/>
+            <OnOff/>
+            <OnOff/>
+
+            <UncontrolledRating />
+            <UncontrolledAccordion titleValue={"Menu"}/>
+            <UncontrolledAccordion titleValue={"Users"}/>
+
+        </div>
+    );
 }
+
+type PageTitlePropsType = {
+
+    title: string
+}
+const PageTitle = (props: PageTitlePropsType) => {
+    return <h1>{props.title}</h1>
+}
+
 
 export default App;
