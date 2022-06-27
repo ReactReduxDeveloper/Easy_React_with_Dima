b99a961a5d5f08a8d0fd204b156469e0195ee7af
ReactReduxDeveloper <cool.pomolov@ya.ru>
2022-06-27 20:07:38 +0300

    KKP_8-9

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Rating from "./components/Rating/Star";
+
 import Accordion from "./components/Accordion/Accordion";
 import {stringify} from "querystring";
 import OnOff from "./components/OnOff/OnOff";
-import UncontrolledAccordion from "./components/Accordion/UncontrolledAccordion";
-import UncontrolledRating from "./components/Rating/Star";
+import {UncontrolledAccordion} from "./components/Accordion/UncontrolledAccordion";
+import {RatingProps, RatingValueType, UncontrolledRating} from "./components/Rating/Star";
 
 
 const App = () => {
+    let [Tvalue, setValue] = useState<RatingValueType>(0)
     return (
         <div className={"App"}>
-          {/*  <OnOff/>
             <OnOff/>
             <OnOff/>
-*/}
+            <OnOff/>
 
+            <UncontrolledRating Tvalue={Tvalue} OnClick={setValue}/>
 
             <UncontrolledAccordion titleValue={"Menu"}/>
-            <UncontrolledAccordion titleValue={"Users"}/>
+
 
         </div>
     );
