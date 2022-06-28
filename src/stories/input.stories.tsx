a6b75ce707f916e16aeb1a5fd37d81db7cc8ee5e
ReactReduxDeveloper <cool.pomolov@ya.ru>
2022-06-28 16:53:36 +0300

    KKP_10-11_SB_Deleting_trash

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,3 +1,13 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import OnOff from "../components/OnOff/OnOff";
 
+export default {
+    title: 'input',
+    component: OnOff
+
+}
+export const UncontrolledInput = ()=> {
+    <input/>
+
+}
